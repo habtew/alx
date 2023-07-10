464db694c59b6db8a5e75875db6e6581c5f18bb3
Habtamu <habtamuwolde92@gmail.com>
2023-07-10 15:44:38 +0530

    mallloc first code

---
+++ alx-low_level_programming/0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(c)*size);
+	if(ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+	}
+
+	return (ptr);
+}
