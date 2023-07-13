3b0207949fd0ddf58a83fbc82a86fda81708efa0
Habtamu <habtamuwolde92@gmail.com>
2023-07-13 16:37:22 +0530

    fixed

--- alx-low_level_programming/0x0C-more_malloc_free/1-string_nconcat.c
+++ alx-low_level_programming/0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,36 +14,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     
     if (n >= len_s2)
     {
-        ptr = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-        for(i = 0; s1[i] != '\0'; i++)
-        {
-            ptr[i] = s1[i];
-            j++;
-        }
-        
-        for(i = 0; s2[i] != '\0'; i++)
-        {
-            ptr[i + j] = s2[i];
-        }
-        
-        return ptr;
+        n = len_s2;
     }
-    else
+    ptr = malloc(sizeof(char) * (strlen(s1) + n + 1));
+    for(i = 0; s1[i] != '\0'; i++)
     {
-        ptr = malloc(sizeof(char) * (strlen(s1) + n + 1));
-        for(i = 0; s1[i] != '\0'; i++)
-        {
             ptr[i] = s1[i];
             j++;
-        }
-        
-        for(i = 0; i < n; i++)
-        {
-            ptr[i + j] = s2[i];
-        }
-        ptr[i+j] = '\0';
-        return (ptr);
     }
-    
-    
+        
+    for(i = 0; i < n; i++)
+    {
+            ptr[i + j] = s2[i];
+    }
+    ptr[i+j] = '\0';
+    return (ptr);}
 }
