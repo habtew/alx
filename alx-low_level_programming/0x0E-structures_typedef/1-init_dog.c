6f353d7618a9f6adeaf4a8dceb46320cf68ede6f
Habtamu <habtamuwolde92@gmail.com>
2023-07-18 21:39:30 +0530

    defined dog in to different varable name

--- alx-low_level_programming/0x0E-structures_typedef/1-init_dog.c
+++ alx-low_level_programming/0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
