14fb3ebce6f13d15d33d0c4bfe3ff24363fca439
Habtamu <habtamuwolde92@gmail.com>
2023-07-19 16:11:33 +0530

    print indef of number if found

--- alx-low_level_programming/0x0F-function_pointers/function_pointers.h
+++ alx-low_level_programming/0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef function_pointers_h
 #define function_pointers_h
+typedef long unsigned int size_t;
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
