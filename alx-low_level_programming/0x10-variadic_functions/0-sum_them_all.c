515b96201842a0f4d274164e6fc3f3865864d38c
Habtamu <habtamuwolde92@gmail.com>
2023-07-20 14:36:56 +0530

    sum of all numbers passed

---
+++ alx-low_level_programming/0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+
+	va_list args;
+	va_start(args, n);
+	
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for(i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end;
+	return (sum);
+}
