24ff0722ea711a863a7d4aace4fc1baed0ce704e
Habtamu <habtamuwolde92@gmail.com>
2023-07-30 21:17:43 +0530

    removed print_before funciton

--- alx-low_level_programming/0x12-singly_linked_lists/100-first.c
+++ alx-low_level_programming/0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_before_main(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
-
 void __attribute__((constructor)) before_main()
 {
-	print_before_main();
+	printf("You're beat! and yet, you must allow,\n");
+        printf("I bore my house upon my back!\n");
+
 }
