43ce9077e9f0b2f678ad6b18067355cd00caa50c
Habtamu <habtamuwolde92@gmail.com>
2023-07-30 21:19:04 +0530

    added print before ain function back

--- alx-low_level_programming/0x12-singly_linked_lists/100-first.c
+++ alx-low_level_programming/0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void __attribute__((constructor)) before_main()
+void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
         printf("I bore my house upon my back!\n");
+
+}
+
+
+void __attribute__((constructor)) before_main()
+{
+	print_before_main();
 
 }
