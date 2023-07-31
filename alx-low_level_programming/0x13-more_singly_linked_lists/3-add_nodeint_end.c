625b0262bf72b27356770d072fa3c1d1cad11e41
Habtamu <habtamuwolde92@gmail.com>
2023-07-31 11:27:51 +0530

    add element at the end of list

---
+++ alx-low_level_programming/0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *current;
+
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		head = new_node;
+		return new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+		return new_node;
+	}
+}
+
