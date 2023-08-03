f8efdef1013e64ea730178d97cdc99b5cd0abd8a
Habtamu <habtamuwolde92@gmail.com>
2023-08-03 14:23:03 +0530

    edited main.h to add _putchar prototype

--- alx-low_level_programming/0x14-bit_manipulation/main.h
+++ alx-low_level_programming/0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _pucthar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
